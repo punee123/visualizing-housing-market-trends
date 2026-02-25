2d85bd7122f6bb75b660911afb2f1f4ef601c0ba
punee123 <veerapuneetha@gmail.com>
2026-02-25 21:33:05 +0530

    Fix indentation for app.run in app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -23,6 +24,6 @@ def docs():
     return render_template('docs.html')
 
 if __name__ == '__main__':
-   port = int(os.environ.get("PORT", 5000))
-
+    # These two lines must have the EXACT same number of spaces (4)
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
